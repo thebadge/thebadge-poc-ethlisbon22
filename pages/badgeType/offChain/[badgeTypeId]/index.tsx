a62b8e7211e42148c0e1dc03f3902b30081069b8
Agustin Pane <agustin.pane@gmail.com>
2022-10-29 10:16:29 +0100

    BadgeTypeId TODO

--- pages/badgeType/offChain/[badgeTypeId]/index.tsx
+++ pages/badgeType/offChain/[badgeTypeId]/index.tsx
@@ -1,7 +1,16 @@
 import { NextPage } from 'next'
+import { useRouter } from 'next/router'
 
 const BadgeTypeId: NextPage = () => {
-  return <>TODO BADGE TYPE PAGE</>
+  const router = useRouter()
+  const badgeTypeId = router.query.badgeTypeId as string
+
+  switch (badgeTypeId) {
+    case '1':
+      return <div>Github strategy page</div>
+    default:
+      return <div>PAGE NOT FOUND</div>
+  }
 }
 
 export default BadgeTypeId
